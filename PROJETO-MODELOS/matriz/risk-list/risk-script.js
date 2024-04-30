70676536ed70e623fcad59438910fc11004962c0
LeandroPS121 <dleandropaulo@gmail.com>
2024-04-30 16:53:05 -0300

    a

--- PROJETO-MODELOS/matriz/risk-list/risk-script.js
+++ PROJETO-MODELOS/matriz/risk-list/risk-script.js
@@ -1,7 +1,15 @@
 function selClick(i){
     for(var j=1; j<=16; j++){
         document.getElementById('rs-'+j).style.backgroundColor = 'white'
+        if(j<14){
+            document.getElementById('r'+j).style.display = 'none'
+        }
     }
     let id_sel = 'rs-'+i
+    let risk = 'r'+i
+    let risk_name = document.getElementById('rd-'+i).innerHTML
     document.getElementById(id_sel).style.backgroundColor = 'black'
+    document.getElementById('risk-name').innerHTML = risk_name
+    //i
+    document.getElementById(risk).style.display = 'block'
 }
