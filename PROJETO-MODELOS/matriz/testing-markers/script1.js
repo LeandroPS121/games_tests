920e6485036289bec41dfc15064f2589f2e7d71e
LeandroPS121 <dleandropaulo@gmail.com>
2024-05-08 10:19:44 -0300

    a

--- PROJETO-MODELOS/matriz/testing-markers/script1.js
+++ PROJETO-MODELOS/matriz/testing-markers/script1.js
@@ -1,9 +1,21 @@
-function createDiv(){
-  let number = document.getElementById('text-test').value
-  if(number != "" && (number>0 && number<10)){
+function addDiv(){
+  var x = document.getElementById('x').value
+  var y = document.getElementById('y').value
+  var intPartX = Math.floor(x)
+  var intPartY = Math.floor(y)
+  var decPartX = x - intPartX
+  var decPartY = y - intPartY
+  if((x>=1 && x<=3)&&(y>=1 && y<=3)){
     const div = document.createElement('div')
-    div.className = "grid-item"
-    div.textContent = 'R1'
-    document.getElementById("risk-cell"+number).appendChild(div)
+    div.className="risk-marker position-relative"
+    alert((decPartX*100)+'%')
+    div.style.left = `${decPartX*100}%`
+    div.style.bottom = `${decPartY*100}%`
+    div.innerHTML = `
+    <svg width="16" height="16" viewBox="0 0 16 16">
+      <circle cx="8" cy="8" r="8"/>
+    </svg>`
+    
+    document.getElementById('risk-cell-'+intPartX+'-'+intPartY).appendChild(div)
   }
 }
