6673530e76d58dda1eade432640c68e0af6a9918
LeandroPS121 <dleandropaulo@gmail.com>
2024-05-15 11:55:43 -0300

    asdasd

---
+++ PROJETO-MODELOS/matriz/testing-add-list-riskFactor/risk-factors.js
@@ -0,0 +1,10 @@
+function addRiskFactor(){
+    const newDivContent = document.createElement('div')
+    newDivContent.className = "row border border-primary mx-0"
+    newDivContent.textContent = 'testano'
+
+    document.getElementById('risk-factor-container').appendChild(newDivContent)
+}
+function delRiskFactor(){
+
+}
