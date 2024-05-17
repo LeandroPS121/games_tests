1a279c772fd34ea5f05ad6f7694a9ef9dfc5a900
LeandroPS121 <dleandropaulo@gmail.com>
2024-05-17 11:51:15 -0300

    a

--- PROJETO-MODELOS/matriz/bolas_teste/script.js
+++ PROJETO-MODELOS/matriz/bolas_teste/script.js
@@ -1,10 +1,17 @@
 function showMarkers(){
-    document.getElementById('R_main');
-    let r2 = document.getElementById('R2');
-    r2.style.display = 'block';
-    r2.className="r_style"
+    let r_markers = document.querySelectorAll('.r_markers')
+    r_markers.forEach(element => {
+        if (element.style.display == 'none'){
+            element.style.display = 'block'
+        } else {
+            element.style.display = 'none'
+        }
+    })
 }
+ 
 function hideMarkers(){
-    let r2 = document.getElementById('R2');
-    r2.style.display = 'none';
+    let r_markers = document.querySelectorAll('.r_markers')
+    r_markers.forEach(element => {
+        element.style.display = 'none'
+    })
 }
