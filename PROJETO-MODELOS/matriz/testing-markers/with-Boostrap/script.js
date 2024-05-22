c14e23b617432ffd0e1b060fb5da03e839b6f91e
LeandroPS121 <dleandropaulo@gmail.com>
2024-05-22 10:20:25 -0300

    deu certo vambora

--- PROJETO-MODELOS/matriz/testing-markers/with-Boostrap/script.js
+++ PROJETO-MODELOS/matriz/testing-markers/with-Boostrap/script.js
@@ -1,109 +1,55 @@
-var count = 1;
+var marker_list1 = [0,0,0,0];
+var marker_list2 = [0,0,0,0];
+var marker_list3 = [0,0,0,0];
+var marker_list4 = [0,0,0,0];
 
-var cell_list1 = [0,0,0,0];
-var cell_list2 = [0,0,0,0];
-var cell_list3 = [0,0,0,0];
-var cell_list4 = [0,0,0,0];
+function addMarker()
+{
+    document.getElementById('espa').setAttribute("data-bs-toggle","tooltip");
+    document.getElementById('espa').setAttribute("data-bs-title","Default tooltip");
 
-// var cell_positionList1 = [50,50,50,50];
-// var cell_positionList2 = [50,50,50,50];
-// var cell_positionList3 = [50,50,50,50];
-// var cell_positionList4 = [50,50,50,50];
+    let x = document.getElementById('x').value;
+    let y = document.getElementById('y').value;
 
-function addMarker(){
-    const x = document.getElementById('x').value;
-    const y = document.getElementById('y').value;
-
-    const decimalX = x - Math.floor(x);
-    const decimalY = y - Math.floor(y);
-
-    const integerX = x - decimalX;
-    const integerY = y - decimalY;
-    
-    const marker_place = document.createElement('div');
-    const marker = document.createElement('div');
-    // const div_container = document.createElement('div');
-    
-    marker.className = "marker";
-    marker.textContent = `R${count}`;
-    marker_place.className = "marker-place";
-    // div_container.className = "container-div d-flex align-items-center justify-content-around border border-primary";
-
-            //////////////////////////////////////////////////
-            // Aplicar na matriz crítica
-            // marker_place.style.left = `${decimalX*100}%`;
-            // marker_place.style.bottom = `${decimalY*100}%`;
-
-            // marker_place.style.left = "50%";
-            // marker_place.style.bottom = "50%";
-
-    // div_container.appendChild(marker_place);
-    marker_place.appendChild(marker);
-
-    div_content = verifyRepeatMarker(integerX,integerY,marker_place);
-
-    document.getElementById('risk-cell-'+integerX+'-'+integerY).appendChild(div_content); //Só considera o valor inteiro na hora do append, (a parte com virgula será utilizada na outra matriz)
-    count++;
-        // addMarkerInCellList(integerX,integerY);
+    const marker = document.createElement('button');
+    marker.className = 'm-2 marker';
+    //acessar o mtx/rm para pegar os riscos
+    marker.textContent = 'ok';
+    marker.title = 'Teste';
+    addMarkerInList(Number(x),Number(y),marker);
 }
 
-function addMarkerInCellList(x,y){
+function addMarkerInList(x,y,marker){
     switch (x){
         case 1:
-            cell_list1[y-1]++;
+            marker_list1[y-1]++;
+            if(marker_list1[y-1]<=9){
+                document.getElementById(`risk-cell-${x}-${y}`).appendChild(marker);
+            }
             break;
         case 2:
-            cell_list2[y-1]++;
+            marker_list2[y-1]++;
+            if(marker_list2[y-1]<=9){
+                document.getElementById(`risk-cell-${x}-${y}`).appendChild(marker);
+            }
             break;
         case 3:
-            cell_list3[y-1]++;
+            marker_list3[y-1]++;
+            if(marker_list3[y-1]<=9){
+                document.getElementById(`risk-cell-${x}-${y}`).appendChild(marker);
+            }
             break;
         case 4:
-            cell_list4[y-1]++;
+            marker_list4[y-1]++;
+            if(marker_list4[y-1]<=9){
+                document.getElementById(`risk-cell-${x}-${y}`).appendChild(marker);
+            }
             break;
     }
 }
-function verifyRepeatMarker(x,y,mk_place){
-    switch (x){
-        case 1:
-            if(cell_list1[y-1] == 0){
-                const div_container = document.createElement('div');
-                div_container.className = "container-div d-flex align-items-center justify-content-around border border-primary";
-                div_container.id = `container_div-${x}-${y}`;
-                return div_container.appendChild(mk_place);
-            } else{
-                return div_container.appendChild(mk_place)
-            }
-            
-        case 2:
-            if(cell_list2[y-1] == 0){
-                const div_container = document.createElement('div');
-                div_container.className = "container-div d-flex align-items-center justify-content-around border border-primary";
-                div_container.id = `container_div-${x}-${y}`;
-                return div_container.appendChild(mk_place);
-            } else{
-                return div_container.appendChild(mk_place)
-            }
-            
-        case 3:
-            if(cell_list3[y-1] == 0){
-                const div_container = document.createElement('div');
-                div_container.className = "container-div d-flex align-items-center justify-content-around border border-primary";
-                div_container.id = `container_div-${x}-${y}`;
-                return div_container.appendChild(mk_place);
-            } else{
-                return div_container.appendChild(mk_place)
-            }
-            
-        case 4:
-            if(cell_list4[y-1] == 0){
-                const div_container = document.createElement('div');
-                div_container.className = "container-div d-flex align-items-center justify-content-around border border-primary";
-                div_container.id = `container_div-${x}-${y}`;
-                return div_container.appendChild(mk_place);
-            } else{
-                return div_container.appendChild(mk_place)
-            }
-            
-    }
+
+function hideTooltip(risk)
+{
+  risk.removeAttribute("data-bs-toggle");
+  risk.removeAttribute("data-bs-title");
 }
